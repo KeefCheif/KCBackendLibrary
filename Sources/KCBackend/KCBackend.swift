4cf4d4fe04feed1f686a774d96277a8b5ce926f4
Keef Cheif <Keefcheif.dev@gmail.com>
2023-01-15 21:30:26 -0700

    first commit to package: test connection to DB by creating a generic test user

--- Sources/KCBackend/KCBackend.swift
+++ Sources/KCBackend/KCBackend.swift
@@ -1,6 +1,12 @@
+import FirebaseAuth
+
 public struct KCBackend {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public static func createAccount() async throws {
+        try await Auth.auth().createUser(withEmail: "test@gmail.com", password: "password")
     }
 }
