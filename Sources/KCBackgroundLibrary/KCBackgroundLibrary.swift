1ba878f997a463eb03d36977efd70f8c5808abca
KeefCheif <KeefCheif.dev@gmail.com>
2024-05-25 17:04:11 -0600

    configure method

--- Sources/KCBackgroundLibrary/KCBackgroundLibrary.swift
+++ Sources/KCBackgroundLibrary/KCBackgroundLibrary.swift
@@ -6,6 +6,10 @@ public class KCBackgroundLibrary {
     public init() {
     }
     
+    public static func configure() {
+        FirebaseApp.configure()
+    }
+    
     public func createAccount() async throws {
         try await Auth.auth().createUser(withEmail: "test@gmail.com", password: "somePassword123!$%")
     }
